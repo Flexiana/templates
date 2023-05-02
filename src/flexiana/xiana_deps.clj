96ac165257694a2dea4becfb7a183d53b8c58d74
Gustavo Valente <gustavomsvalente@gmail.com>
2023-05-02 13:06:20 -0300

    [dev] POC custom template with options

--- src/flexiana/xiana_deps.clj
+++ src/flexiana/xiana_deps.clj
@@ -1,4 +1,5 @@
-(ns flexiana.xiana-deps)
+(ns flexiana.xiana-deps
+  (:require [clojure.pprint :as pp]))
 
 (defn data-fn
   "Example data-fn handler.
@@ -14,6 +15,15 @@
 
   Result is used as the EDN for the template."
   [edn data]
+  
   ;; must return the whole EDN hash map
-  (println "template-fn returning edn")
-  edn)
+  (pp/pprint data)
+
+  (pp/pprint edn)
+
+  (if (:electric data)
+    (update edn :transform into
+            [["dummy" "src/{{top/file}}"
+              {"electric.tmpl" "electric.clj"} :only]])
+    edn))
+
